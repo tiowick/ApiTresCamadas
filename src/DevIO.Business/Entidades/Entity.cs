e1987dcbad2dc1e405ff7f5a3ac86f782018897e
jeferson <jefinho368@gmail.com>
2024-12-30 11:33:05 -0300

    Adicionar arquivos de projeto.

---
+++ src/DevIO.Business/Entidades/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Entidades
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid(); //sempre um id novo
+            
+        }
+
+        public Guid Id { get; set; }
+
+
+
+    }
+}
---
+++ src/DevIO.Business/Entidades/Fornecedor.cs
@@ -0,0 +1,22 @@
+﻿using DevIO.Business.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Entidades
+{
+    public class Fornecedor : Entity
+    {
+        public string? Nome { get; set; }
+
+        public string? Documento { get; set; }
+
+        public TipoFornecedor TipoFornecedor { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public Endereco? Endereco { get; set; }
+    }
+}
