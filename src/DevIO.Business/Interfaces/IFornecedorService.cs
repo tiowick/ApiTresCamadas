5eb14c6aecc0331781f3ef6f6018306876044fff
jeferson <jefinho368@gmail.com>
2024-12-30 12:18:30 -0300

    Definindo interfaces de serviços

---
+++ src/DevIO.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,18 @@
+﻿using DevIO.Business.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IFornecedorService : IDisposable
+    {
+        Task Adicionar(Fornecedor fornecedor);
+
+        Task Atualizar(Fornecedor fornecedor);
+
+        Task Remover(Guid id);
+    }
+}
---
+++ src/DevIO.Business/Interfaces/IProdutoService.cs
@@ -0,0 +1,18 @@
+﻿using DevIO.Business.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IProdutoService
+    {
+        Task Adicionar(Produto produto);
+
+        Task Atualizar(Produto produto);
+
+        Task Remover(Guid id);
+    }
+}
