03d7198d67d47acf8d70f50033875ef00411bb7a
jeferson <jefinho368@gmail.com>
2024-12-31 11:46:35 -0300

    Customizando comportamentos do EF

--- src/DevIO.Data/Context/AppDbContext.cs
+++ src/DevIO.Data/Context/AppDbContext.cs
@@ -12,7 +12,8 @@ namespace DevIO.Data.Context
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            // configs futuras
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
         }
         // mapeando entidades de negocios
 
@@ -36,6 +37,27 @@ namespace DevIO.Data.Context
 
             base.OnModelCreating(modelBuilder);
         }
+
+       public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+       {
+
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken); 
+        
+       }
+
 
     }
 }
