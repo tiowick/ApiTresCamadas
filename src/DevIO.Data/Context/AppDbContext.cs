2b5da05a234a607178150635280320c893911c13
jeferson <jefinho368@gmail.com>
2024-12-31 10:57:20 -0300

    Definindo o contexto do Entity Framework

---
+++ src/DevIO.Data/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DevIO.Business.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            // configs futuras
+        }
+        // mapeando entidades de negocios
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+    }
+}
