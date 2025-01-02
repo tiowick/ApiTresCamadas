14f6bf5bf2f6d17c167a89f729d4b55d546f13fd
jeferson <jefinho368@gmail.com>
2025-01-02 18:49:08 -0300

    Repositorio generic + Especializando as entidades + Geral Camada de api

---
+++ src/DevIO.Data/Repositorio/FornecedorRepositorio.cs
@@ -0,0 +1,47 @@
+﻿using DevIO.Business.Entidades;
+using DevIO.Business.Interfaces;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Repositorio
+{
+    public class FornecedorRepositorio : Repositorio<Fornecedor>, IFornecedorRepositorio
+    {
+        public FornecedorRepositorio(AppDbContext context) : base(context) 
+        {
+            
+        }
+
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(c => c.Endereco)
+                .Include(c => c.Produtos)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Endereco?> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await Db.Enderecos.AsNoTracking()
+                .FirstOrDefaultAsync(f => f.FornecedorId == fornecedorId);
+        }
+
+        public async Task RemoverEnderecoPorFornecedor(Endereco endereco)
+        {
+            Db.Enderecos.Remove(endereco);
+            await SaveChanges();
+        }
+    }
+}
---
+++ src/DevIO.Data/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using DevIO.Business.Entidades;
+using DevIO.Business.Interfaces;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Repositorio
+{
+    public class ProdutoRepositorio : Repositorio<Produto>, IProdutoRepositorio
+    {
+
+        public ProdutoRepositorio(AppDbContext context) : base(context)
+        {
+            
+        }
+
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await Db.Produtos.AsNoTracking()
+                .Include(f => f.Fornecedor)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await Db.Produtos.AsNoTracking()
+                .Include(f => f.Fornecedor)
+                .OrderBy(p => p.Nome)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+    }
+}
